1f8273d60121075d50aff22b4bc8e7914d53f91c
brian <brian@IDDQD>
2022-05-22 18:46:22 -0700

    Add project files.

---
+++ SkyheimExtended/Class1.cs
@@ -0,0 +1,102 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+using UnityEngine;
+using skyheim;
+
+
+namespace SkyheimExtended
+{
+    [BepInPlugin("posixone.SkyheimExtended", "SkyheimExtended", "1.0.3")]
+    [BepInProcess("valheim.exe")]
+    public class SkyheimExtended : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("posixone.SkyheimExtended");
+
+        public static ConfigEntry<float> manaRegen;
+        public static ConfigEntry<float> maxMana;
+
+        void Awake()
+        {
+            manaRegen = Config.Bind("Global", "manaRegen", 3f);
+            maxMana = Config.Bind("Global", "maxMana", 100f);
+
+            harmony.PatchAll();
+        }
+
+
+        [HarmonyPatch(typeof(skyheim.SkyheimMana), nameof(skyheim.SkyheimMana.UpdateMana))]
+
+
+        class SkyheimMana_Patch
+        {
+
+            //public static readonly ItemDrop.ItemData itemData = Player.m_localPlayer.GetCurrentWeapon();
+
+            static void Postfix(ref float ____manaRegen, ref float ____maxMana)
+            {
+                //ItemDrop.ItemData currentWeapon = Player.m_localPlayer.GetCurrentWeapon();
+
+                //if (currentWeapon.m_dropPrefab.name == "rune_frostbolt")
+                //{
+                //____manaRegen = manaRegen.Value;
+                //____maxMana = maxMana.Value;
+                //}              
+
+
+                //Only run code when weapon is equipped
+                if ((Object)(object)Player.m_localPlayer != (Object)null)
+                {
+                    ItemDrop.ItemData currentWeapon = Player.m_localPlayer.GetCurrentWeapon();
+
+                    //float frostbolt_skillLevel = ((Player.m_localPlayer.GetSkillFactor((Skills.SkillType.Swim)) * 100f) + 0.000001f);
+                    float playerLevel = Player.m_localPlayer.GetLevel();
+
+
+                    if (currentWeapon != null && (Object)(object)currentWeapon.m_dropPrefab != (Object)null)
+                    {
+                        string equipped = currentWeapon.m_dropPrefab.name;
+                        
+
+                        //Debug.Log($"Current Weapon: {equipped}");
+
+                        //if the Skyheim Frostbolt Rune is equipped
+                        if (equipped == "rune_frostbolt")
+                        {
+                            ____manaRegen = manaRegen.Value;
+                            ____maxMana = maxMana.Value;
+                            Debug.Log($"Skill Level: {playerLevel}");
+                        }
+                    }
+                        //bool skyheim_engaged = true;
+                        
+
+                }   
+
+                //if (skyheim_engaged = true)
+                //{
+                //    ItemDrop.ItemData currentWeapon = Player.m_localPlayer.GetCurrentWeapon();
+                //    ____manaRegen = manaRegen.Value;
+                //    ____maxMana = maxMana.Value;
+                //    Debug.Log($"Weapon: {currentWeapon.m_dropPrefab.name}");
+                //    Debug.Log($"Mana: {____currentMana}");
+                //}
+
+                //else if (currentWeapon.m_dropPrefab.name == "rune_firebolt")
+                //{
+                //    ____manaRegen = manaRegen.Value;
+                //____maxMana = maxMana.Value;
+                // }
+
+
+                //else
+                //{
+                //    ____manaRegen = 3f;
+                //____maxMana = 100f;
+                //}
+
+                //Debug.Log($"Skyheim Extended: {se_GetCurrentWeapon()}");         
+            }
+        }
+    }
+}
